8aff3ee30a540d406e8705b4a0a7cb2f67e8fc8b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2022-07-11 12:54:45 +0530

    Delete E-Loan.Entities directory

--- E-Loan.Entities/BusinessStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace E_Loan.Entities
-{
-    public enum BusinessStatus
-    {
-        Individual = 1,
-        Organisation = 2
-    }
-}
--- E-Loan.Entities/Response.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace E_Loan.Entities
-{
-    public class Response
-    {
-        public string Status { get; set; }
-        public string Message { get; set; }
-    }
-}
